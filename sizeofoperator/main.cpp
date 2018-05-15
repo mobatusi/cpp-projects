7ac41cfb5c1fbdee7d781fed7192b7137889e917
Dolu Obatusin <mobatusin.tcsec@gmail.com>
2018-05-15 11:26:41 -0400

    variable types

--- sizeofoperator/main.cpp
+++ sizeofoperator/main.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main() {
+  cout << "Minuimum values: " << endl;
+  cout << "char: " << CHAR_MIN << endl;
   cout << "int: " << INT_MIN << endl;
-
-return 0
+  cout << "short: " << SHRT_MIN << endl;
+  cout << "long: " << LONG_MIN << endl;
+  cout << "long long: " << LLONG_MIN << endl;
+//
+  cout << "=================================" << endl;
+  cout << "Maximum values: " << endl;
+  cout << "char: " << CHAR_MAX << endl;
+  cout << "int: " << INT_MAX << endl;
+  cout << "short: " << SHRT_MAX << endl;
+  cout << "long: " << LONG_MAX << endl;
+  cout << "long long: " << LLONG_MIN << endl;
+//
+return 0;
 }
